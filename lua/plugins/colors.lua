4223c5fb15330c92780ff739d46f2ce1dbe1ae01
Pascal Boehlen <pboehlen@student.ethz.ch>
2026-01-25 14:48:36 +0800

    added which-key and some colorschemes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,23 +1,65 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      transparent_background = true,
-      integrations = {
-        neotree = true,
-        treesitter = true,
-        telescope = {
-          enabled = true,
-          style = "nvchad" -- Optional: makes telescope look extra clean
-        },
-        -- Add other plugins here as you install them
-      },
-    })
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+				integrations = {
+					neotree = true,
+					treesitter = true,
+					telescope = {
+						enabled = true,
+						style = "nvchad", -- Optional: makes telescope look extra clean
+					},
+				},
+			})
 
-    -- setup must be called before loading the colorscheme
-    vim.cmd.colorscheme "catppuccin"
-    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "none", bg = "none" })
-  end
+			-- setup must be called before loading the colorscheme
+			vim.cmd.colorscheme("catppuccin")
+			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "none", bg = "none" })
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		name = "tokyonight",
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent_background = true,
+				integrations = {
+					neotree = true,
+					treesitter = true,
+					telescope = {
+						enabled = true,
+						style = "nvchad", -- Optional: makes telescope look extra clean
+					},
+				},
+			})
+		end,
+	},
+	{
+		"sainnhe/everforest",
+		name = "everforest",
+		priority = 1000,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				transparent_background = true,
+				integrations = {
+					neotree = true,
+					treesitter = true,
+					telescope = {
+						enabled = true,
+						style = "nvchad", -- Optional: makes telescope look extra clean
+					},
+				},
+			})
+		end,
+	},
 }
