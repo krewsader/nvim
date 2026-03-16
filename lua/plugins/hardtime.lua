366b564f672b6396c1cf6b641dd0b96f3ab36cb3
Pascal Boehlen <pboehlen@student.ethz.ch>
2026-03-17 00:04:43 +0800

    disabled hardtime and added shortcut to telescope to go through notifications

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,6 +1,7 @@
 return { -- Plugin that forces you to properly learn vim motions, blocking keypresses and giving hints
-	"m4xshen/hardtime.nvim",
-	lazy = false,
-	dependencies = { "MunifTanjim/nui.nvim" },
-	opts = {},
+ "m4xshen/hardtime.nvim",
+ 	lazy = false,
+  enabled = false,
+ 	dependencies = { "MunifTanjim/nui.nvim" },
+ 	opts = {},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return { -- Adds a fuzzy finder to nvim to quickly move through files
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fn', ":Telescope notify<CR>")
     end
   },
   {
