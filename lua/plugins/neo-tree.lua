975722b1929cd16887a440abd8302217f33d10ba
Pascal Boehlen <pboehlen@student.ethz.ch>
2026-03-17 14:01:12 +0800

    changed keybinds

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -14,8 +14,8 @@ return { -- Adds a file tree system on the left to navigate files more easily
       },
     })
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-    vim.keymap.set("n", "<C-Left>", ":vertical resize +2<CR>", { silent = true })
-    vim.keymap.set("n", "<C-Right>", ":vertical resize -2<CR>", { silent = true })
+    vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { silent = true })
+    vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { silent = true })
     vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true })
   end
 }
