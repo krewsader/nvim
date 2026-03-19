c730900d255f96bb18fdd9f063f8d10a321f230f
Pascal Boehlen <pboehlen@student.ethz.ch>
2026-03-19 15:41:18 +0800

    fixed treesitter-context

--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -1,3 +1,12 @@
 return { -- plugin to display function context
-  "nvim-treesitter/nvim-treesitter-context";
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    max_lines = 3,           -- How many lines the window should span
+    min_window_height = 0,   -- Minimum editor window height to enable context
+    line_numbers = true,
+    multiline_threshold = 20, -- Maximum number of lines to show for a single context
+    trim_scope = 'outer',    -- Which context lines to discard if max_lines is exceeded
+    mode = 'cursor',         -- Line used to calculate context. Can be 'cursor' or 'topline'
+  },
 }
