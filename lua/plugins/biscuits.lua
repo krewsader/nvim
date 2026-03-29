0cd7116d90c9e74ad4ed5c083d0ffc562cebc497
Pascal Boehlen <pboehlen@student.ethz.ch>
2026-03-29 11:12:00 +0800

    added biscuits to get context about closing brackets

---
+++ lua/plugins/biscuits.lua
@@ -0,0 +1,9 @@
+return { -- Plugin that displays context for curly brackets
+  'code-biscuits/nvim-biscuits',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  }, -- Plugin that displays context for curly brackets
+  opts = {
+    -- Config goes here
+  }
+}
