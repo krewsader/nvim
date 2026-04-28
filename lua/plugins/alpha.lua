ab23a19e5e73e4075f88e3ae90d7a6d75ecc5793
Pascal Boehlen <pboehlen@student.ethz.ch>
2026-04-28 21:24:31 +0800

    added autochdir functionality and dashboard/greeter

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,15 @@
+return   { -- Greeter/Dashboard
+    "goolord/alpha-nvim",
+    -- dependencies = { 'nvim-mini/mini.icons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      local theta = require("alpha.themes.theta")
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(
+        theta.config
+      )
+    end,
+  }
